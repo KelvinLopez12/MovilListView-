e35d8b5ea398dbebcb32d0d4e1f6b7eb45fedffd
KelvinLopez12 <59572986+KelvinLopez12@users.noreply.github.com>
2023-11-08 22:06:22 -0500

    Tarea de ListView y StateFull Widget

---
+++ lista/lib/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.yellowAccent;
+const secondColor = Color.fromARGB(255, 255, 59, 59);
+
+const fromTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
+
+const dateTextStyle = TextStyle(
+  color: Color.fromARGB(255, 59, 72, 255),
+  fontSize: 15.0,
+  fontWeight: FontWeight.w300,
+);
+
+const subjectTextStyle = TextStyle(
+  fontSize: 17.0,
+  fontWeight: FontWeight.w500,
+);
+
+const bodyTextStyle = TextStyle(
+  fontSize: 16.0,
+);
